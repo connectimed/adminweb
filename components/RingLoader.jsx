bad7cee1d979c9302e5a1f88b4e3ca053f46d330
lastborncoltd <lastborn.auth@gmail.com>
2025-08-17 15:43:42 +0300

    firebase deploy

--- components/RingLoader.jsx
+++ components/RingLoader.jsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 import React from "react";
 
 const RingLoader = () => {
   return (
-    <div className="flex items-center justify-center">
-      <span className="loading loading-spinner loading-sm"></span>
+    <div className="flex h-[60vh] w-screen items-center justify-center">
+      <Image
+        className="h-10 w-10 animate-spin"
+        src="/images/loading.svg"
+        height={512}
+        width={512}
+        alt="loading icon"
+      />
     </div>
   );
 };
--- components/UnAuthorized.jsx
+++ components/UnAuthorized.jsx
@@ -18,7 +18,7 @@ const UnAuthorized = () => {
             alt="error icon"
           />
         </div>
-        <h1 className="mt-8 text-5xl font-semibold tracking-tight text-balance text-dark-bg sm:text-7xl">
+        <h1 className="mt-8 text-4xl font-semibold tracking-tight text-balance text-dark-bg sm:text-5xl">
           Access Denied
         </h1>
         <p className="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
@@ -29,13 +29,13 @@ const UnAuthorized = () => {
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <Link
             href="https://www.imedconnect.or.tz"
-            className="rounded-md bg-indigo-500 px-4 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            className="rounded-md bg-indigo-500 px-4 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
           >
             Go To Main
           </Link>
           <div
             onClick={logOut}
-            className="rounded-md text-sm font-semibold text-dark border border-dark px-4 py-2.5 cursor-pointer"
+            className="rounded-md text-sm font-semibold text-dark border border-dark px-4 py-2 cursor-pointer"
           >
             Log out <span aria-hidden="true">&rarr;</span>
           </div>
