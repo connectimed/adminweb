c1cf30388f0dd5116e0c5d80e0f0b71152d916b8
lastborncoltd <lastborn.auth@gmail.com>
2026-03-31 22:08:49 +0300

    feat: add EntrepreneursPanel to admin dashboard

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -6,6 +6,7 @@ import { ModulesPanel } from "@/components/ModulesPanel";
 import { ApplicationsPanel } from "@/components/ApplicationsPanel";
 import UnAuthorized from "@/components/UnAuthorized";
 import { ExaminationsPanel } from "@/components/ExaminationsPanel";
+import { EntrepreneursPanel } from "@/components/EntrepreneursPanel";
 
 export default function Home() {
   const { userData, logOut } = UserAuth();
@@ -18,14 +19,16 @@ export default function Home() {
     <div className="container max-w-7xl mx-auto">
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8">
         <UsersPanel />
-
         <ModulesPanel />
       </div>
 
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8 mt-8">
         <ApplicationsPanel />
-
         <ExaminationsPanel />
+      </div>
+
+      <div className="mt-8">
+        <EntrepreneursPanel />
       </div>
     </div>
   );
